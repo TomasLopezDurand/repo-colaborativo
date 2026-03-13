fffccedaa67b17ea6d6680d6771ecb113e215b20
TomasLopezDurand <tlopezdurand@udesa.edu.ar>
2026-03-13 17:44:09 -0300

    Update funciones_habitos.py

--- funciones_habitos.py
+++ funciones_habitos.py
@@ -1,5 +1,14 @@
 def registrar_habitos():
-    
+    '''
+    Función que genera una lista y pide al usuario que ingrese actividades diarias.
+    Las actividades diarias se guardan como string dentro de la lista "actividades".
+
+    Returns
+    -------
+    actividades : lista
+        Guarda las actividades diarias del usuario.
+
+    '''
     seguir = "si"
     actividades = []
     while seguir == "si":
@@ -9,6 +18,20 @@ def registrar_habitos():
     return actividades
 
 def analizar_habitos(lista_actividades):
+    '''
+    En base a la función registrar_habitos, esta función cuenta la cantidad de veces que se repite una actividad en la lista y se guardan en el diccionario.
+
+    Parameters
+    ----------
+    lista_actividades : lista
+        Guarda las actividades diarias del usuario..
+
+    Returns
+    -------
+    analisis : diccionario
+        Como clave tiene el nombre de las actividades y el valor es el número de veces que se repite la palabra dentro de la lista de actividades.
+
+    '''
     analisis = {}
     for nombre_actividad in lista_actividades:
         if nombre_actividad not in analisis:
