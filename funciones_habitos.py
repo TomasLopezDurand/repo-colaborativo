fade15a52b546afae304a8b6d39bf78d29b1c5e3
TomasLopezDurand <tlopezdurand@udesa.edu.ar>
2026-03-13 17:39:01 -0300

    Update funciones_habitos.py

--- funciones_habitos.py
+++ funciones_habitos.py
@@ -1,14 +1,16 @@
 def registrar_habitos():
+    
     seguir = "si"
     actividades = []
     while seguir == "si":
         actividad = input("Ingrese una actividad diara: ")
         actividades.append(actividad)
         seguir = input("Quiere seguir ingresando? (si/no) ")
+    return actividades
 
-def analizar_habitos(actividades):
+def analizar_habitos(lista_actividades):
     analisis = {}
-    for nombre_actividad in actividades:
+    for nombre_actividad in lista_actividades:
         if nombre_actividad not in analisis:
             analisis[nombre_actividad] = 1
         else:
